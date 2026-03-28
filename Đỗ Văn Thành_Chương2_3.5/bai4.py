21b040936ccad24e49c1b2e62ff0ec907f390eba
20224159eaut <20224159@eaut.edu.vn>
2026-03-28 11:43:42 +0700

    Add files via upload

---
+++ Đỗ Văn Thành_Chương2_3.5/bai4.py
@@ -0,0 +1,11 @@
+n = int(input("Nhap so nguyen duong: "))
+
+if n % 2 == 0 and n % 3 == 0:
+    print("So chia het cho ca 2 va 3")
+elif n % 2 == 0:
+    print("So chia het cho 2")
+elif n % 3 == 0:
+    print("So chia het cho 3")
+else:
+    print("So khong chia het cho 2 va 3")
+    
